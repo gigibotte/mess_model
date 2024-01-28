b3e15be3033520a40a3df83573124780fd0da390
Luigi Bottecchia <gigi.bottecchia@gmail.com>
2024-01-28 19:44:19 +0100

    ReRunModel

---
+++ src/core/feasibility.jl
@@ -0,0 +1,15 @@
+module Feasibility
+
+export feasibility
+
+using Main.SystemStructMess
+
+using Main.ModelConfiguration: timestep, timespan
+
+using Main.Dirs: path
+
+using DataFrames, CSV
+
+
+
+end
